9535b2aefcc2a9815a7fd16ff1af3ce4e25a4c19
Sunil <sunilyahoo95@gmail.com>
2022-06-29 00:19:44 +0530

    add text style, add spanable string effect

--- app/src/main/java/com/compui/MainActivity.kt
+++ app/src/main/java/com/compui/MainActivity.kt
@@ -5,13 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,12 +16,19 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.compui.ui.theme.ComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +36,9 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
+
+            //---------------------------------------------------------------------//
+            //->Row and Column
 
             //Row View
             /*Row(
@@ -47,10 +53,9 @@ class MainActivity : ComponentActivity() {
                 Text(text = " To")
                 Text(text = " Here")
             }*/
-            
-            
-            //Column View
 
+
+            //Column View
             //Nested view chains
             /*Column(
                 modifier = Modifier
@@ -78,18 +83,72 @@ class MainActivity : ComponentActivity() {
                 Text(text = "Info")
             }*/
 
-        val painter = painterResource(id = R.drawable.office_man)
-        val description = "Welcome to Here"
-        val title = "It's pleasure to meet you"
 
-         Box(
-             modifier = Modifier
-                 .fillMaxSize(0.5f)
-                 .padding(15.dp)
-         ) {
-             ImageCard(painter = painter, contentDescription = description, title = title)
-         }
+            //---------------------------------------------------------------------//
+            //->Composable Image Card
 
+            /*val painter = painterResource(id = R.drawable.office_man)
+            val description = "Welcome to Here"
+            val title = "It's pleasure to meet you"
+
+             Box(
+                 modifier = Modifier
+                     .fillMaxSize(0.5f)
+                     .padding(15.dp)
+             ) {
+                 ImageCard(painter = painter, contentDescription = description, title = title)
+             }*/
+
+
+            //---------------------------------------------------------------------//
+            //->Text Styling
+
+            val fontFamily = FontFamily(
+                Font(R.font.lexend_thin, FontWeight.Thin),
+                Font(R.font.lexend_regular, FontWeight.Normal),
+                Font(R.font.lexend_light, FontWeight.Light),
+                Font(R.font.lexend_medium, FontWeight.Medium),
+                Font(R.font.lexend_bold, FontWeight.Bold),
+                Font(R.font.lexend_extrabold, FontWeight.ExtraBold)
+            )
+
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
+            ) {
+                Text(
+                    //Use AnnotatedString method to add Style to String Characters
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Cyan,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Cyan,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
+            }
 
         }
     }
@@ -98,10 +157,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ImageCard(
-    painter:Painter,
+    painter: Painter,
     contentDescription: String,
-    title:String,
-    modifier:Modifier = Modifier
+    title: String,
+    modifier: Modifier = Modifier
 ) {
     Card(
         modifier = modifier.fillMaxWidth(),
@@ -113,27 +172,33 @@ fun ImageCard(
             Image(
                 painter = painter,
                 contentDescription = contentDescription,
-                contentScale = ContentScale.Crop)
+                contentScale = ContentScale.Crop
+            )
         }
 
-        Box(modifier = Modifier
-            .height(200.dp)
-            .background(
-                Brush.verticalGradient(
-                    colors = listOf(
-                        Color.Transparent,
-                        Color.Black),
-                    startY = 300f     // The Position of Y-axis from where gardient need to apply
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f     // The Position of Y-axis from where gardient need to apply
+                    )
                 )
-            )) {
+        ) {
 
         }
 
         //Here we put Text Inside to Box, Bcuz we need Textview placement above the Image widget
-        Box(modifier = modifier
-            .height(200.dp)
-            .padding(12.dp),
-            contentAlignment = Alignment.BottomStart) {
+        Box(
+            modifier = modifier
+                .height(200.dp)
+                .padding(12.dp),
+            contentAlignment = Alignment.BottomStart
+        ) {
             Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
         }
     }
@@ -148,6 +213,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     //ComposeSampleTheme {
-        Greeting(",welcome to Compose ")
+    Greeting(",welcome to Compose ")
     //}
 }
